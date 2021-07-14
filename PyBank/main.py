bd762a24f0e35a6c425a3dd9a0aa386bc076d56c
Bgood524 <bgood524@gmail.com>
2021-07-14 17:52:43 -0500

    total number of months

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,13 @@
+#Print the budget data csv file
 import os
 import csv
+from typing import Counter
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 print(csvpath)
+
+month = []
+
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -10,6 +15,15 @@ with open(csvpath) as csvfile:
     print(csvreader)
 
     csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    months = 0
+    NetTotal = 0
+    for row in csvreader:
+        
+    
 
-    for row in csvreader: 
-        print(row)
+        
+#Total number of months
+        months+=1
+        print(months)
+#Total Profit/Losses
