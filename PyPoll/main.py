e4a52055425b38388f2d59432b1344962a412fbb
Bgood524 <bgood524@gmail.com>
2021-07-20 15:05:40 -0500

    PyPoll script to count candidates, create percentages, and return outputs in text

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,11 +10,52 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
+    #Total Votes Cast
+    election_df = list(csvreader)
+    row_count =  len(election_df)
+
+     
     
+
+
+    #Create a list of candidates to loop through data and count each candidate's votes
     candidates = []
 
 
     
-    for row in csvreader:
+    for row in election_df:
         candidate = row[2]
         candidates.append(candidate)
+
+    Khan_count = candidates.count("Khan")
+    Correy_count = candidates.count("Correy")
+    Li_count = candidates.count("Li")
+    OTooley_count = candidates.count("O'Tooley")
+
+    print(OTooley_count)
+
+    #get individual percentages
+    Khan_percentage = Khan_count / row_count * 100
+    Correy_percentage = Correy_count / row_count * 100
+    Li_percentage = Li_count / row_count * 100
+    OTooley_percentage = OTooley_count / row_count * 100
+
+    candidates = ["Khan", "Correy", "Li", "O'Tooley"]
+    votes = [Khan_count, Correy_count, Li_count, OTooley_count]
+
+    candidates_and_votes = dict(zip(candidates,votes))
+    winner = max(candidates_and_votes, key = candidates_and_votes.get)
+
+
+#output
+    print("---------------------------------------------------------")
+print("Election Results")
+print("----------------------------------------------------------")
+print("Total Votes: " + str(row_count))
+print("Khan: " + str(Khan_percentage) + "% ("  + str(Khan_count) + ")")
+print("Correy: " + str(Correy_percentage) + "% ("  + str(Correy_count) + ")")
+print("Li: " + str(Li_percentage) + "% ("  + str(Li_count) + ")")
+print("O'Tooley: " + str(OTooley_percentage) + "% ("  + str(OTooley_count) + ")")
+print("----------------------------------------------------------")
+print("Winner:" + str(winner))
+print("----------------------------------------------------------")
